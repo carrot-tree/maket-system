afeea870dd25cffb5c4f119d16c4d25bde2c80dd
carrot <carrot@github.com>
2017-11-21 18:13:09 +0800

    new

--- checkup.php
+++ checkup.php
@@ -1,4 +1,3 @@
-
 <?php
 header("Content-type: text/html; charset=utf-8");
 //连接数据库
@@ -28,96 +27,129 @@ if ($db->connect_errno<> 0)
 
 <style type="text/css">
 	.top{
+			border-style: groove;
 			margin: 0 auto;
-			width: 60%;
-			height: 200px;
+			width: 70%;
+			height: 510px;
 			text-align: center;
 		}
+	.g_jpg{
+		
+		width: 70%;
+		height: auto;
+		margin: 0 auto;
+
+	}
+	div img{
+		width: 100%;
+		height: auto;
+		object-fit: cover;
+	}
+	.title_in{
+		margin:0 auto;
+		width: 80%;
+	}
+	.in_in{
+		width: 33%
+		text-align: center;
+	}
 </style>
 </head>
 
 <body>
 	<div class="top">
-	<h2 class="title">输入商品号和数量和方式</h1>
-	<form class="info" action="#" method="post">
-		<input class="inp" type="text" name="g_no">
-		<input class="inp" type="text" name="amount">
-		<input class="inp" type="text" name="style">
-		<input type="submit" name="yes" value="确定">
-	</form>
-	<form class="info" action="#" method="post">
-		
-		<input type="submit" name="get" value="get">
-	</form>
-	<form class="info" action="#" method="post">
-		
-		<input type="submit" name="finishi" value="完成">
-	</form>
-	
-	<?php
+		<div class="in">
+		<h2 class="title">输入商品号和数量和方式</h1>
+			<div class="title_in">
+				<form class="info" action="#" method="post">
+				<div class="in_in">
+					<h3>商品号<br></h3><input class="inp" type="text" name="g_no">
+				</div>
+				<div class="in_in">
+					<h3>数量<br></h3><input class="inp" type="text" name="amount">
+				</div>
+				<div class="in_in">
+					<h3>方式<br></h3><input class="inp" type="text" name="style">
 
-	if (isset($_POST['get'])) 
-	{
-		session_start();
-		$price_all = 0;
-		$_SESSION["price_all"] = $price_all;
-		$admin = $_SESSION["admin"];
-		$sql4 = "INSERT INTO purchase (time,cashier) values (now(),'$admin')";
-		$result4 = $db->query($sql4);
-		var_dump($result4);
-	}
-	if (isset($_POST['yes'])) 
-	{
-
-		//往purchase表插入一条数据
-		$g_no = (int)$_POST['g_no'];
-		$amount = (int)$_POST['amount'];
-		$style = $_POST['style'];
-		session_start();
-		$admin = $_SESSION["admin"];
-		$t=date("Y-m-d H:i:s");
-		$sql = "SELECT price FROM goods where g_no=$g_no";
-		$result = $db->query($sql);
-		$rows = $result->fetch_array(MYSQLI_ASSOC);
-		$price1 = (float)$rows["price"];
-		$price2 = $price1 * $amount;
+				</div>
+					<br>
+					<input type="submit" name="get" value="get">
+					<input type="submit" name="yes" value="确定">
+					<input type="submit" name="finishi" value="完成">
+				</form>
+			</div>
 		
-		$price_all = $_SESSION["price_all"];
-		$price_all += $price2;
+		</div>
 		
+		<?php
 
-		var_dump($g_no,$amount,$style,$admin,$t,$rows["price"],$price1);
-		var_dump($price2);
-		//$sql0 = "INSERT INTO purchase (style, cashier, price, time) values ( '$style', '$admin', '$price2', now())";
-		//$result0 = $db->query($sql0);
-	
-		$sql2 = "SELECT p_no FROM purchase ORDER BY p_no desc";
-		$result2 = $db->query($sql2);
-		$rows2 = $result2->fetch_array(MYSQLI_ASSOC);
-		$p_no = (int)$rows2["p_no"];
-		//往orders表插入一条数据
-		$sql3 = "INSERT INTO orders (g_no, p_no, num) values ('$g_no', $p_no ,'$amount')";
-		$db->query($sql3);
+		if (isset($_POST['get'])) 
+		{
+			session_start();
+			$price_all = 0;
+			$_SESSION["price_all"] = $price_all;
+			$admin = $_SESSION["admin"];
+			$sql4 = "INSERT INTO purchase (time,cashier) values (now(),'$admin')";
+			$result4 = $db->query($sql4);
+			var_dump($result4);
+		}
+		if (isset($_POST['yes'])) 
+		{
+
+			//往purchase表插入一条数据
+			$g_no = (int)$_POST['g_no'];
+			$amount = (int)$_POST['amount'];
+			$style = $_POST['style'];
+			session_start();
+			$admin = $_SESSION["admin"];
+			$t=date("Y-m-d H:i:s");
+			$sql = "SELECT price FROM goods where g_no=$g_no";
+			$result = $db->query($sql);
+			$rows = $result->fetch_array(MYSQLI_ASSOC);
+			$price1 = (float)$rows["price"];
+			$price2 = $price1 * $amount;
+			
+			$price_all = $_SESSION["price_all"];
+			$price_all += $price2;
+			
+
+			//var_dump($g_no,$amount,$style,$admin,$t,$rows["price"],$price1);
+			var_dump($price2);
+			
 		
-		$_SESSION["price_all"] = $price_all;
-		$_SESSION["style"] = $style;
-		$_SESSION["p_no"] = $p_no;
+			$sql2 = "SELECT p_no FROM purchase ORDER BY p_no desc";
+			$result2 = $db->query($sql2);
+			$rows2 = $result2->fetch_array(MYSQLI_ASSOC);
+			$p_no = (int)$rows2["p_no"];
+			//往orders表插入一条数据
+			$sql3 = "INSERT INTO orders (g_no, p_no, num) values ('$g_no', $p_no ,'$amount')";
+			$db->query($sql3);
+			
+			$_SESSION["price_all"] = $price_all;
+			$_SESSION["style"] = $style;
+			$_SESSION["p_no"] = $p_no;
 
-	}
-	if (isset($_POST['finishi'])) 
-	{
-		session_start();
-		$price_all = $_SESSION["price_all"];
-		$style = $_SESSION["style"];
-		$p_no = $_SESSION["p_no"];
-		$sql_f = "UPDATE `maket`.`purchase` set style='$style', price='$price_all' where p_no='$p_no' ";
-		$result_f = $db->query($sql_f);
-		var_dump($price_all,$style,$p_no);
-		var_dump($result_f);
-		
-	}
 
-	?>
+		}
+		if (isset($_POST['finishi'])) 
+		{
+			session_start();
+			$price_all = $_SESSION["price_all"];
+			$style = $_SESSION["style"];
+			$p_no = $_SESSION["p_no"];
+			$sql_f = "UPDATE `maket`.`purchase` set style='$style', price='$price_all' where p_no='$p_no' ";
+			$result_f = $db->query($sql_f);
+			//var_dump($result_f);
+			echo "<h2>本次总计消费".$price_all."元</h2>";
+			echo "<h2>欢迎下次光临</h2>";
+			
+		}
+
+		?>
+	</div>
+	<div class= g_jpg>
+		<img src="goods.jpg"  alt="商品表" />
+
 	</div>
 
 
--- mouth.php
+++ mouth.php
@@ -1,3 +1,101 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+//连接数据库
+$host = '127.0.0.1';
+$user = 'root';
+$pwd = '';
+$dbname = 'maket';
+
+$db = new mysqli($host,$user,$pwd,$dbname);
+$db->query("SET NAMES UTF8");
+
+if ($db->connect_errno<> 0)
+{
+	die("连接失败<br>");
+
+}
+
+//上午销量
+$sql0 = "SELECT date(p.time) as 日期上午,SUM(num) as 销量
+FROM goods g,purchase p,orders o
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no and HOUR(p.time) BETWEEN 7 and 11
+GROUP BY date(p.time)";
+
+$result0 = $db->query($sql0);
+$rows0 = [];
+while($row0 = $result0->fetch_array(MYSQLI_ASSOC))
+{
+	$rows0[] = $row0;
+}
+//中午
+$sql1 = "SELECT date(p.time) as 日期中午,SUM(num) as 销量
+FROM goods g,purchase p,orders o
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no and HOUR(p.time) BETWEEN 11 and 15
+GROUP BY date(p.time)";
+$result1 = $db->query($sql1);
+$rows1 = [];
+while($row1 = $result1->fetch_array(MYSQLI_ASSOC))
+{
+	$rows1[] = $row1;
+}
+//下午
+$sql2 = "SELECT date(p.time) as 日期下午,SUM(num) as 销量
+FROM goods g,purchase p,orders o
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no and HOUR(p.time) BETWEEN 15 and 19
+GROUP BY date(p.time)";
+$result2 = $db->query($sql2);
+$rows2 = [];
+while($row2 = $result2->fetch_array(MYSQLI_ASSOC))
+{
+	$rows2[] = $row2;
+}
+
+//每月利润排行
+$sql3 = "SELECT month(p.time) as 月份,name as 商品名,SUM(num) 销量,SUM(num)*(g.price-g.cost) as 盈利
+from orders o,goods g,purchase p 
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no GROUP BY o.g_no,month(p.time)
+ORDER BY month(p.time) asc,盈利 desc";
+$result3 = $db->query($sql3);
+$rows3 = [];
+while($row3 = $result3->fetch_array(MYSQLI_ASSOC))
+{
+	$rows3[] = $row3;
+}
+//每月最佳销售
+$sql4 = "SELECT  best.月份,best.商品名,MAX(best.`销量`) as 最佳销量 FROM 
+(
+SELECT `name` as 商品名,SUM(num) as 销量 ,month(p.time) as 月份 from orders o,goods g,purchase p 
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no 
+GROUP BY o.g_no,month(p.time) 
+ORDER BY MONTH(p.time),SUM(num) desc
+) 
+as best
+GROUP BY 月份";
+$result4 = $db->query($sql4);
+$rows4 = [];
+while($row4 = $result4->fetch_array(MYSQLI_ASSOC))
+{
+	$rows4[] = $row4;
+}
+//最佳电话订购
+
+$sql5 = "SELECT g.`name` AS 商品名,SUM(o.num) AS 销量
+FROM goods g,purchase p,orders o
+WHERE  g.g_no=o.g_no and p.p_no=o.p_no and p.style = 1
+GROUP BY 商品名
+ORDER BY 销量 DESC
+LIMIT 1
+";
+$result5 = $db->query($sql5);
+$rows5 = [];
+while($row5 = $result5->fetch_array(MYSQLI_ASSOC))
+{
+	$rows5[] = $row5;
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="zh-cmn-Hans">
 <head>
@@ -17,12 +115,275 @@ var s = document.getElementsByTagName("script")[0];
 s.parentNode.insertBefore(hm, s);
 })();
 </script>
+<style type="text/css">
+
+</style>
 
 </head>
 
 <body>
 
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div class="span4">
+			<h2>
+				时段销量排行
+			</h2>
+			<div class="tabbable" id="tabs-982858">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a data-toggle="tab" href="#panel-483247">上午销量</a>
+					</li>
+					<li>
+						<a data-toggle="tab" href="#panel-604444">中午销量</a>
+					</li>
+					<li>
+						<a data-toggle="tab" href="#panel-604445">下午销量</a>
+					</li>
 
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-483247">
+						
+						<p>
+							<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>
+										日期
+									</th>
+									<th>
+										销量
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								foreach ($rows0 as $row0 ) {
+								?>
+								
+								<tr class="success">
+									<td>
+										<?php echo $row0['日期上午'];?>
+									</td>
+									<td>
+										<?php echo $row0['销量'];?>
+									</td>									
+								</tr>
+								<?php
+								}
+								?>	
+							</tbody>
+							</table>
+						</p>
+					</div>
+				
+					<div class="tab-pane" id="panel-604444">
+						<p>
+							<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>
+										日期
+									</th>
+									<th>
+										销量
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								foreach ($rows1 as $row1 ) {
+								?>
+								
+								<tr class="warning">
+									<td >
+										<?php echo $row1['日期中午'];?>
+									</td>
+									<td>
+										<?php echo $row1['销量'];?>
+									</td>									
+								</tr>
+								<?php
+								}
+								?>								
+							</tbody>
+							</table>
+						</p>
+					</div>
+					<div class="tab-pane" id="panel-604445">
+						<p>
+							<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>
+										日期
+									</th>
+									<th>
+										销量
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								foreach ($rows2 as $row2 ) {
+								?>
+								
+								<tr class="danger">
+									<td >
+										<?php echo $row2['日期下午'];?>
+									</td>
+									<td>
+										<?php echo $row2['销量'];?>
+									</td>									
+								</tr>
+								<?php
+								}
+								?>															
+							</tbody>
+							</table>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row-fluid">
+				<div class="span12">
+				</div>
+			
+		<div class="span4">
+			<h2>
+				每月利润排行
+			</h2>
+			<table class="table table-bordered table-hover">
+				<thead>
+					<tr>
+						<th>
+							月份
+						</th>
+						<th>
+							商品
+						</th>
+						<th>
+							销量
+						</th>
+						<th>
+							盈利
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					foreach ($rows3 as $row3 ) {
+					?>
+					<tr class="info">
+						<td>
+							<?php echo $row3['月份'];?>
+						</td>
+						<td>
+							<?php echo $row3['商品名'];?>
+						</td>
+						<td>
+							<?php echo $row3['销量'];?>
+						</td>
+						<td>
+							<?php echo $row3['盈利'];?>
+						</td>
+					</tr>
+					<?php
+					}
+					?>	
+				</tbody>
+			</table>
+		</div>
+		<div class="span4">
+			<h2>
+				Best Selling
+			</h2>
+			<div class="tabbable" id="tabs-233050">
+				<ul class="nav nav-tabs">
+					<li class="active">
+						<a data-toggle="tab" href="#panel-649236">By Phone</a>
+					</li>
+					<li>
+						<a data-toggle="tab" href="#panel-146678">Monthly</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-649236">
+						<p>
+							<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>
+										商品名
+									</th>
+									<th>
+										销量
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								foreach ($rows5 as $row5 ) {
+								?>
+								<tr class="success">				
+									<td>
+										<?php echo $row5['商品名'];?>
+									</td>
+									<td>
+										<?php echo $row5['销量'];?>
+									</td>
+								</tr>
+								<?php
+								}
+								?>									
+							</tbody>
+							</table>
+						</p>
+					</div>
+					<div class="tab-pane" id="panel-146678">
+						<p>
+							<table class="table table-bordered table-hover">
+							<thead>
+								<tr>
+									<th>
+										月份
+									</th>
+									<th>
+										商品名
+									</th>
+									<th>
+										最佳销量
+									</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								foreach ($rows4 as $row4 ) {
+								?>
+								<tr class="warning">
+									<td>
+										<?php echo $row4['月份'];?>
+									</td>
+									<td>
+										<?php echo $row4['商品名'];?>
+									</td>
+									<td>
+										<?php echo $row4['最佳销量'];?>
+									</td>
+								</tr>
+								<?php
+								}
+								?>									
+							</tbody>
+							</table>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 
 
